c2da31260769ddb81d009f031e4f5c23d0fc0818
Payton Swick <payton@foolord.com>
2017-11-15 16:35:07 -0500

    Add DisallowGlobalFunctionsSniff

---
+++ NeutronStandard/Sniffs/Globals/DisallowGlobalFunctionsSniff.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NeutronStandard\Sniffs\Globals;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class DisallowGlobalFunctionsSniff implements Sniff {
+	public function register() {
+		return [T_FUNCTION];
+	}
+
+	public function process(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$currentToken = $tokens[$stackPtr];
+		if (! empty($currentToken['conditions'])) {
+			return;
+		}
+		echo "currentToken " .json_encode($currentToken['conditions']) . "\n";
+		$functionName = $phpcsFile->getDeclarationName($stackPtr);
+		$error = 'Global functions are not allowed';
+		$phpcsFile->addError($error, $stackPtr, 'GlobalFunctions');
+	}
+}
+
+
