d84f6cdf2da6ee81b0d8be62f1363f5cdb58c5fb
Payton Swick <payton@foolord.com>
2017-11-18 09:30:06 -0500

    Add DisallowMagicSerializeSniff

---
+++ NeutronStandard/Sniffs/MagicMethods/DisallowMagicSerializeSniff.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NeutronStandard\Sniffs\MagicMethods;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class DisallowMagicSerializeSniff implements Sniff {
+	public function register() {
+		return [T_FUNCTION];
+	}
+
+	public function process(File $phpcsFile, $stackPtr) {
+		$functionName = $phpcsFile->getDeclarationName($stackPtr);
+		if ($functionName === '__serialize') {
+			$error = 'Magic serialize is not allowed';
+			$phpcsFile->addError($error, $stackPtr, 'MagicSerialize');
+		}
+	}
+}
+
+
