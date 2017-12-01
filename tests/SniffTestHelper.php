fa8a4640e6e23824de76abe89c1f6d79d9c13fbc
Payton Swick <payton@foolord.com>
2017-11-30 19:27:24 -0500

    Add getLineNumbersFromMessages to SniffTestHelper

--- tests/SniffTestHelper.php
+++ tests/SniffTestHelper.php
@@ -8,12 +8,16 @@ use PHP_CodeSniffer\Ruleset;
 use PHP_CodeSniffer\Config;
 
 class SniffTestHelper {
-	public function getTestLocalFile(string $sniffFile, string $fixtureFile) : LocalFile {
+	public function getTestLocalFile(string $sniffFile, string $fixtureFile): LocalFile {
 		$config = new Config();
 		$config->cache = false;
 		$ruleset = new Ruleset($config);
 		$ruleset->registerSniffs([$sniffFile], [], []);
 		$ruleset->populateTokenListeners();
 		return new LocalFile($fixtureFile, $ruleset, $config);
+	}
+
+	public function getLineNumbersFromMessages(array $messages): array {
+		return array_keys($messages);
 	}
 }
