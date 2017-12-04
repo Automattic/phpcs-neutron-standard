483b0694b334a5aebe6f229324bf4cca31ac9fa8
Payton Swick <payton@foolord.com>
2017-12-04 10:27:21 -0500

    Add more tests for RequireStrictTypesSniff

--- tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
+++ tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
@@ -6,7 +6,7 @@ namespace NeutronStandardTest;
 use PHPUnit\Framework\TestCase;
 
 class RequireStrictTypesSniffTest extends TestCase {
-	public function testRequireStrictTypes() {
+	public function testRequireStrictTypesIfMissing() {
 		$fixtureFile = __DIR__ . '/fixture.php';
 		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
 
@@ -16,5 +16,29 @@ class RequireStrictTypesSniffTest extends TestCase {
 		$foundErrors = $phpcsFile->getErrors();
 		$lines = $helper->getLineNumbersFromMessages($foundErrors);
 		$this->assertEquals([1], $lines);
+	}
+
+	public function testRequireStrictTypesPassesIfPresentOnNewLine() {
+		$fixtureFile = __DIR__ . '/fixture2.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->getTestLocalFile($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$foundErrors = $phpcsFile->getErrors();
+		$lines = $helper->getLineNumbersFromMessages($foundErrors);
+		$this->assertEquals([], $lines);
+	}
+
+	public function testRequireStrictTypesPassesIfPresentOnSameLine() {
+		$fixtureFile = __DIR__ . '/fixture3.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->getTestLocalFile($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$foundErrors = $phpcsFile->getErrors();
+		$lines = $helper->getLineNumbersFromMessages($foundErrors);
+		$this->assertEquals([], $lines);
 	}
 }
---
+++ tests/Sniffs/StrictTypes/fixture3.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+function test() {
+	define("FOO", "bar");
+}
+
+class MyClass {
+	public function notTooLong() {
+		$foo = 'bar';
+		$foo;
+		$foo; // Hello
+	}
+}
