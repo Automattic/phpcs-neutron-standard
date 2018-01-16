ff1056980888e97d711a4a6ed57abfe84c5553d1
Payton Swick <payton@foolord.com>
2018-01-16 17:30:52 -0500

    RequireStrictTypesSniff: add tests for ignoring interfaces

--- tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
+++ tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
@@ -49,4 +49,15 @@ class RequireStrictTypesSniffTest extends TestCase {
 		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([], $lines);
 	}
+
+	public function testRequireStrictTypesIgnoresInterfaceFiles() {
+		$fixtureFile = __DIR__ . '/interfaceOnlyFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
+		$this->assertEquals([], $lines);
+	}
 }
---
+++ tests/Sniffs/StrictTypes/interfaceOnlyFixture.php
@@ -0,0 +1,5 @@
+<?php
+
+interface MyInterface {
+	public function doSomething();
+}
