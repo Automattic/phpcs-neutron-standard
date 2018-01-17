8099e756e891efd7ad2021705ec352bffb12a2eb
Payton Swick <payton@foolord.com>
2018-01-17 12:47:01 -0500

    RequireStrictTypesSniff: ignore for interfaces (#24)
    * RequireStrictTypesSniff: add tests for ignoring interfaces
    
    * RequireStrictTypesSniff: add test for interface and class together
    
    * RequireStrictTypesSniff: ignore rule for interface-only files

--- tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
+++ tests/Sniffs/StrictTypes/RequireStrictTypesSniffTest.php
@@ -49,4 +49,26 @@ class RequireStrictTypesSniffTest extends TestCase {
 		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([], $lines);
 	}
+
+	public function testRequireStrictTypesIgnoresInterfaceFiles() {
+		$fixtureFile = __DIR__ . '/interfaceOnlyFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
+		$this->assertEquals([], $lines);
+	}
+
+	public function testRequireStrictTypesDoesNotIgnoreInterfaceAndClass() {
+		$fixtureFile = __DIR__ . '/interfaceAndClassFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/StrictTypes/RequireStrictTypesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
+		$this->assertEquals([1], $lines);
+	}
 }
