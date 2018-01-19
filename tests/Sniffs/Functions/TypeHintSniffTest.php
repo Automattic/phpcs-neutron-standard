b258efdcde58335f3d22b077cc676dde5d7a789c
Payton Swick <payton@foolord.com>
2018-01-19 12:27:27 -0500

    TypeHintSniff: improve argument type detection to support variadic types (#26)
    * TypeHintSniff: add test for variadic arguments
    
    * TypeHintSniff: improve argument type detection
    
    * TypeHintSniff: add more variadic argument tests

--- tests/Sniffs/Functions/TypeHintSniffTest.php
+++ tests/Sniffs/Functions/TypeHintSniffTest.php
@@ -16,4 +16,15 @@ class TypeHintSniffTest extends TestCase {
 		$lines = $helper->getWarningLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([118, 123, 128, 133, 139, 145, 159, 216, 221], $lines);
 	}
+
+	public function testTypeHintSniffWithVariadicArgs() {
+		$fixtureFile = __DIR__ . '/variadicArgumentsFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/Functions/TypeHintSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$lines = $helper->getWarningLineNumbersFromFile($phpcsFile);
+		$this->assertEquals([10, 19, 24, 29], $lines);
+	}
 }
