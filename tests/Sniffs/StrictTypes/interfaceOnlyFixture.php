c98e926fb26f8c11b9931322c193de2ba83adce8
Payton Swick <payton@foolord.com>
2018-01-23 14:25:33 -0500

    Tests: add better interfaceOnlyFixture

--- tests/Sniffs/StrictTypes/interfaceOnlyFixture.php
+++ tests/Sniffs/StrictTypes/interfaceOnlyFixture.php
@@ -1,5 +1,27 @@
 <?php
 
-interface MyInterface {
-	public function doSomething();
+namespace Just\Some\Testing;
+
+use Brain\Nonces\NonceInterface;
+
+/**
+ * Interface SettingsPageAuthInterface
+ */
+interface SettingsPageAuthInterface {
+	/**
+	 * @param array $request_data
+	 *
+	 * @return bool
+	 */
+	public function isAllowed(array $request_data = []): bool;
+
+	/**
+	 * @return NonceInterface
+	 */
+	public function nonce(): NonceInterface;
+
+	/**
+	 * @return string
+	 */
+	public function cap(): string;
 }
