dd1e0dbe63dd517150df39040fbf52bdd72991fc
Payton Swick <payton@foolord.com>
2018-01-26 16:09:13 -0500

    Tests: DisallowGlobalFunctionsSniff: allow global functions in a namespace

--- tests/Sniffs/Globals/DisallowGlobalFunctionsSniffTest.php
+++ tests/Sniffs/Globals/DisallowGlobalFunctionsSniffTest.php
@@ -16,4 +16,15 @@ class DisallowGlobalFunctionsSniffTest extends TestCase {
 		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([3], $lines);
 	}
+
+	public function testDisallowGlobalFunctionsSniffAllowsNamespacedGlobals() {
+		$fixtureFile = __DIR__ . '/NamespacedGlobalFunctionsFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/Globals/DisallowGlobalFunctionsSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
+		$this->assertEquals([], $lines);
+	}
 }
