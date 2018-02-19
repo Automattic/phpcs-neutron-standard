b2392ac55904db9295a6cda1fe399413c9fe31e8
Payton Swick <payton@foolord.com>
2018-02-19 14:19:36 -0500

    Add DisallowAssignAlignSniff

--- NeutronStandard/Sniffs/AssignAlign/DisallowAssignAlignSniff.php
+++ NeutronStandard/Sniffs/AssignAlign/DisallowAssignAlignSniff.php
@@ -8,9 +8,21 @@ use PHP_CodeSniffer\Files\File;
 
 class DisallowAssignAlignSniff implements Sniff {
 	public function register() {
-		return [];
+		return [T_WHITESPACE];
 	}
 
 	public function process(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		if (strlen($tokens[$stackPtr]['content']) > 1) {
+			$assignOperators = [
+				T_EQUAL,
+				T_DOUBLE_ARROW,
+			];
+			$nextNonWhitespacePtr = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true, null, false);
+			if ($nextNonWhitespacePtr !== false && in_array($tokens[$nextNonWhitespacePtr]['code'], $assignOperators, true)) {
+				$error = 'Assignment alignment is not allowed';
+				$phpcsFile->addWarning($error, $stackPtr, 'Aligned');
+			}
+		}
 	}
 }
