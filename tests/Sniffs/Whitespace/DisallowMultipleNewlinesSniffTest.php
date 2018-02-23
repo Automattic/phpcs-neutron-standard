31e5464807a8222bbd19e0a0a9e63db32f25fdcf
Payton Swick <payton@foolord.com>
2018-02-23 13:26:46 -0500

    Add fixing to DisallowMultipleNewlinesSniff (#51)
    * Tests: add test for fixing DisallowMultipleNewlinesSniff
    
    * Add fixing to DisallowMultipleNewlinesSniff

--- tests/Sniffs/Whitespace/DisallowMultipleNewlinesSniffTest.php
+++ tests/Sniffs/Whitespace/DisallowMultipleNewlinesSniffTest.php
@@ -15,4 +15,17 @@ class DisallowMultipleNewlinesSniffTest extends TestCase {
 		$lines = $helper->getErrorLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([8, 15, 20], $lines);
 	}
+
+	public function testFixDisallowMultipleNewlinesSniff() {
+		$fixtureFile = __DIR__ . '/NoMultipleNewlinesFixture.php';
+		$fixedFixtureFile = __DIR__ . '/FixedMultipleNewlinesFixture.php';
+		$sniffFile = __DIR__ . '/../../../NeutronStandard/Sniffs/Whitespace/DisallowMultipleNewlinesSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs($sniffFile, $fixtureFile);
+		$phpcsFile->process();
+		$actualContents = $helper->getFixedFileContents($phpcsFile);
+		$fixedContents = file_get_contents($fixedFixtureFile);
+		$this->assertEquals($fixedContents, $actualContents);
+	}
 }
