f3b642fa75573ee8555eca97d78c6bce1efce256
Payton Swick <payton@foolord.com>
2018-02-27 18:41:06 -0500

    Add DisallowAssignAlignSniff fixing

--- NeutronStandard/Sniffs/AssignAlign/DisallowAssignAlignSniff.php
+++ NeutronStandard/Sniffs/AssignAlign/DisallowAssignAlignSniff.php
@@ -13,16 +13,31 @@ class DisallowAssignAlignSniff implements Sniff {
 
 	public function process(File $phpcsFile, $stackPtr) {
 		$tokens = $phpcsFile->getTokens();
-		if (strlen($tokens[$stackPtr]['content']) > 1) {
-			$assignOperators = [
-				T_EQUAL,
-				T_DOUBLE_ARROW,
-			];
-			$nextNonWhitespacePtr = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true, null, false);
-			if ($nextNonWhitespacePtr !== false && in_array($tokens[$nextNonWhitespacePtr]['code'], $assignOperators, true)) {
-				$error = 'Assignment alignment is not allowed';
-				$phpcsFile->addWarning($error, $stackPtr, 'Aligned');
+		// If the next non-whitespace after multiples spaces is an equal sign or double arrow, mark a warning
+		if (strlen($tokens[$stackPtr]['content']) <= 1) {
+			return;
+		}
+		$nextNonWhitespacePtr = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true, null, false);
+		if ($nextNonWhitespacePtr !== false && $this->isTokenAnAssignment($tokens[$nextNonWhitespacePtr])) {
+			$error = 'Assignment alignment is not allowed';
+			$shouldFix = $phpcsFile->addFixableWarning($error, $stackPtr, 'Aligned');
+			if ($shouldFix) {
+				$this->fixTokens($phpcsFile, $stackPtr);
 			}
 		}
+	}
+
+	private function isTokenAnAssignment($token) {
+		$assignOperators = [
+			T_EQUAL,
+			T_DOUBLE_ARROW,
+		];
+		return in_array($token['code'], $assignOperators, true);
+	}
+
+	private function fixTokens(File $phpcsFile, $stackPtr) {
+		$phpcsFile->fixer->beginChangeset();
+		$phpcsFile->fixer->replaceToken($stackPtr, ' ');
+		$phpcsFile->fixer->endChangeset();
 	}
 }
