eb05a8b2ecd2e3db4e4d1f0d6a097e748ea89ea0
Payton Swick <payton@foolord.com>
2018-03-02 14:16:38 -0500

    Add RequireNewlineBetweenFunctionsSniff

--- NeutronStandard/Sniffs/Whitespace/RequireNewlineBetweenFunctionsSniff.php
+++ NeutronStandard/Sniffs/Whitespace/RequireNewlineBetweenFunctionsSniff.php
@@ -8,12 +8,46 @@ use PHP_CodeSniffer\Files\File;
 
 class RequireNewlineBetweenFunctionsSniff implements Sniff {
 	public function register() {
-		return [T_WHITESPACE];
+		return [T_FUNCTION];
 	}
 
 	public function process(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$helper = new SniffHelpers();
+		$endofFuncPtr = $helper->getEndOfFunctionPtr($phpcsFile, $stackPtr);
+		if (! $endofFuncPtr) {
+			return;
+		}
+		$endOfLinePtr = $helper->getNextNewlinePtr($phpcsFile, $endofFuncPtr);
+		if (! $endOfLinePtr) {
+			return;
+		}
+		$nextToken = $tokens[$endOfLinePtr + 1];
+		if (! isset($nextToken['content'])) {
+			return;
+		}
+		if ($nextToken['content'] === "\n") {
+			return;
+		}
+		// Only trigger if the next line contains a function definition
+		$nextEndOfLinePtr = $helper->getNextNewlinePtr($phpcsFile, $endofFuncPtr + 1);
+		$nextFunctionPtr = $phpcsFile->findNext([T_FUNCTION], $endOfLinePtr + 1, null, false, null, true);
+		if (! $nextFunctionPtr || $nextFunctionPtr > $nextEndOfLinePtr) {
+			return;
+		}
+
+		$error = 'Functions must be separated by a blank line';
+		$shouldFix = $phpcsFile->addFixableError($error, $endofFuncPtr, 'MissingNewline');
+		if ($shouldFix) {
+			$this->fixTokens($phpcsFile, $stackPtr);
+		}
 	}
 
 	private function fixTokens(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$endofFuncPtr = $helper->getEndOfFunctionPtr($phpcsFile, $stackPtr);
+		$phpcsFile->fixer->beginChangeset();
+		$phpcsFile->fixer->addNewline($endofFuncPtr);
+		$phpcsFile->fixer->endChangeset();
 	}
 }
