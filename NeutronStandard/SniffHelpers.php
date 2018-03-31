d74f97c7a4c47b5cc9cc37bf8bcd493a8f8aa0fe
Payton Swick <payton@foolord.com>
2018-03-31 14:51:01 -0400

    RequireImportsSniff: implement sniff

--- NeutronStandard/SniffHelpers.php
+++ NeutronStandard/SniffHelpers.php
@@ -21,6 +21,62 @@ class SniffHelpers {
 		return true;
 	}
 
+	public function isMethodCall(File $phpcsFile, $stackPtr) {
+		if (! $this->isFunctionCall($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		$tokens = $phpcsFile->getTokens();
+		$prevPtr = $phpcsFile->findPrevious([T_OBJECT_OPERATOR], $stackPtr - 1, $stackPtr - 2);
+		if ($prevPtr && isset($tokens[$prevPtr])) {
+			return true;
+		}
+		return false;
+	}
+
+	public function isPropertyAccess(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$prevPtr = $phpcsFile->findPrevious([T_OBJECT_OPERATOR], $stackPtr - 1, $stackPtr - 2);
+		if ($prevPtr && isset($tokens[$prevPtr])) {
+			return true;
+		}
+		return false;
+	}
+
+	public function isObjectInstantiation(File $phpcsFile, $stackPtr) {
+		if (! $this->isFunctionCall($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		$tokens = $phpcsFile->getTokens();
+		$prevPtr = $phpcsFile->findPrevious([T_NEW], $stackPtr - 1, $stackPtr - 2);
+		if ($prevPtr && isset($tokens[$prevPtr])) {
+			return true;
+		}
+		return false;
+	}
+
+	// Borrowed this idea from https://pear.php.net/reference/PHP_CodeSniffer-3.1.1/apidoc/PHP_CodeSniffer/LowercasePHPFunctionsSniff.html
+	public function isBuiltInFunction(File $phpcsFile, $stackPtr) {
+		$allFunctions = get_defined_functions();
+		$builtInFunctions = array_flip($allFunctions['internal']);
+		$tokens = $phpcsFile->getTokens();
+		$functionName = $tokens[$stackPtr]['content'];
+		return isset($builtInFunctions[strtolower($functionName)]);
+	}
+
+	public function isPredefinedConstant(File $phpcsFile, $stackPtr) {
+		$allConstants = get_defined_constants();
+		$tokens = $phpcsFile->getTokens();
+		$constantName = $tokens[$stackPtr]['content'];
+		return isset($allConstants[$constantName]);
+	}
+
+	public function isPredefinedClass(File $phpcsFile, $stackPtr) {
+		$allClasses = get_declared_classes();
+		$tokens = $phpcsFile->getTokens();
+		$className = $tokens[$stackPtr]['content'];
+		return in_array($className, $allClasses);
+	}
+
 	// From https://stackoverflow.com/questions/619610/whats-the-most-efficient-test-of-whether-a-php-string-ends-with-another-string
 	public function doesStringEndWith(string $string, string $test): bool {
 		$strlen = strlen($string);
@@ -104,5 +160,156 @@ class SniffHelpers {
 			return true;
 		}
 		return ! $openFunctionBracketPtr;
+	}
+
+	public function getImportType(File $phpcsFile, $stackPtr): string {
+		$tokens = $phpcsFile->getTokens();
+		$nextStringPtr = $phpcsFile->findNext([T_STRING], $stackPtr + 1);
+		if (! $nextStringPtr) {
+			return 'unknown';
+		}
+		$nextString = $tokens[$nextStringPtr];
+		if ($nextString['content'] === 'function') {
+			return 'function';
+		}
+		if ($nextString['content'] === 'const') {
+			return 'const';
+		}
+		return 'class';
+	}
+
+	public function getImportNames(File $phpcsFile, $stackPtr): array {
+		$tokens = $phpcsFile->getTokens();
+		$nextStringOrBracketPtr = $phpcsFile->findNext([T_STRING, T_OPEN_USE_GROUP], $stackPtr + 1);
+		if (! $nextStringOrBracketPtr || ! isset($tokens[$nextStringOrBracketPtr])) {
+			return [];
+		}
+		if ($tokens[$nextStringOrBracketPtr]['type'] === 'T_OPEN_USE_GROUP') {
+			$endBracketPtr = $phpcsFile->findNext([T_CLOSE_USE_GROUP], $nextStringOrBracketPtr + 1);
+			if (! $endBracketPtr) {
+				return [];
+			}
+			return $this->getAllStringsBefore($phpcsFile, $nextStringOrBracketPtr + 1, $endBracketPtr);
+		}
+		$endOfStatementPtr = $phpcsFile->findNext([T_SEMICOLON], $nextStringOrBracketPtr + 1);
+		$nextSeparatorPtr = $phpcsFile->findNext([T_NS_SEPARATOR], $nextStringOrBracketPtr + 1, $endOfStatementPtr);
+		if ($nextSeparatorPtr) {
+			return $this->getImportNames($phpcsFile, $nextSeparatorPtr);
+		}
+		return [$tokens[$nextStringOrBracketPtr]['content']];
+	}
+
+	public function getAllStringsBefore(File $phpcsFile, int $startPtr, int $endPtr): array {
+		$tokens = $phpcsFile->getTokens();
+		$strings = [];
+		$nextStringPtr = $phpcsFile->findNext([T_STRING], $startPtr);
+		while ($nextStringPtr < $endPtr) {
+			if (! $nextStringPtr || ! isset($tokens[$nextStringPtr])) {
+				break;
+			}
+			$nextString = $tokens[$nextStringPtr];
+			$strings[] = $nextString['content'];
+			$nextStringPtr = $phpcsFile->findNext([T_STRING], $nextStringPtr + 1);
+		}
+		return $strings;
+	}
+
+	public function isClass(File $phpcsFile, $stackPtr): bool {
+		$nextSeparatorPtr = $phpcsFile->findNext([T_NS_SEPARATOR], $stackPtr + 1, $stackPtr + 2);
+		if ($nextSeparatorPtr) {
+			return false;
+		}
+		$prevSeparatorPtr = $phpcsFile->findPrevious([T_NS_SEPARATOR], $stackPtr - 1, $stackPtr - 2);
+		if ($prevSeparatorPtr) {
+			return false;
+		}
+		$previousStatementPtr = $phpcsFile->findPrevious([T_SEMICOLON, T_CLOSE_CURLY_BRACKET], $stackPtr - 1);
+		if (! $previousStatementPtr) {
+			$previousStatementPtr = 1;
+		}
+		if ($this->isConstant($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		if ($this->isMethodCall($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		if ($this->isPropertyAccess($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		if ($this->isBuiltInFunction($phpcsFile, $stackPtr)) {
+			return false;
+		}
+		$prevUsePtr = $phpcsFile->findPrevious([T_USE, T_CONST, T_FUNCTION], $stackPtr - 1, $previousStatementPtr);
+		if ($prevUsePtr) {
+			return false;
+		}
+		return true;
+	}
+
+	public function isConstant(File $phpcsFile, $stackPtr): bool {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		$stringName = $token['content'];
+		if (strtoupper($stringName) !== $stringName) {
+			return false;
+		}
+		$nextSeparatorPtr = $phpcsFile->findNext([T_NS_SEPARATOR], $stackPtr + 1, $stackPtr + 2);
+		if ($nextSeparatorPtr) {
+			return false;
+		}
+		$prevSeparatorPtr = $phpcsFile->findPrevious([T_NS_SEPARATOR], $stackPtr - 1, $stackPtr - 2);
+		if ($prevSeparatorPtr) {
+			return false;
+		}
+		$previousStatementPtr = $phpcsFile->findPrevious([T_SEMICOLON, T_CLOSE_CURLY_BRACKET], $stackPtr - 1);
+		if (! $previousStatementPtr) {
+			$previousStatementPtr = 1;
+		}
+		$prevUsePtr = $phpcsFile->findPrevious([T_USE], $stackPtr - 1, $previousStatementPtr);
+		if ($prevUsePtr) {
+			return false;
+		}
+		return true;
+	}
+
+	public function isConstantDefinition(File $phpcsFile, $stackPtr): bool {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		$prevNonWhitespacePtr = $phpcsFile->findPrevious(T_WHITESPACE, $stackPtr - 1, $stackPtr - 3, true, null, false);
+		if (! $prevNonWhitespacePtr || ! isset($tokens[$prevNonWhitespacePtr])) {
+			return false;
+		}
+		$prevToken = $tokens[$prevNonWhitespacePtr];
+		if ($prevToken['content'] === 'const') {
+			return true;
+		}
+		return false;
+	}
+
+	public function getConstantName(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$nextStringPtr = $phpcsFile->findNext([T_STRING], $stackPtr + 1, $stackPtr + 3);
+		if (! $nextStringPtr || ! isset($tokens[$nextStringPtr])) {
+			return null;
+		}
+		return $tokens[$nextStringPtr]['content'];
+	}
+
+	public function isStaticFunctionCall(File $phpcsFile, $stackPtr): bool {
+		return (bool) $this->getStaticPropertyClass($phpcsFile, $stackPtr);
+	}
+
+	public function getStaticPropertyClass(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		if (isset($tokens[$stackPtr - 1]['type']) && $tokens[$stackPtr - 1]['type'] === 'T_DOUBLE_COLON' && isset($tokens[$stackPtr - 2]['content'])) {
+			return $tokens[$stackPtr - 2]['content'];
+		}
+		return null;
+	}
+
+	public function isFunctionAMethod(File $phpcsFile, $stackPtr): bool {
+		$tokens = $phpcsFile->getTokens();
+		$currentToken = $tokens[$stackPtr];
+		return ! empty($currentToken['conditions']);
 	}
 }
--- NeutronStandard/Sniffs/Imports/RequireImportsSniff.php
+++ NeutronStandard/Sniffs/Imports/RequireImportsSniff.php
@@ -7,12 +7,151 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
 
 class RequireImportsSniff implements Sniff {
+	private $importedFunctions = [];
+	private $importedConsts = [];
+	private $importedClasses = [];
+
 	public function register() {
-		return [];
+		return [T_USE, T_STRING];
 	}
 
 	public function process(File $phpcsFile, $stackPtr) {
-		$error = 'Function imports must be explicitly imported';
-		$phpcsFile->addError($error, $stackPtr, 'Function');
+		$helper = new SniffHelpers();
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		if ($token['type'] === 'T_USE') {
+			return $this->processUse($phpcsFile, $stackPtr);
+		}
+		if ($helper->isStaticFunctionCall($phpcsFile, $stackPtr)) {
+			return $this->processStaticFunctionCall($phpcsFile, $stackPtr);
+		}
+		if ($helper->isFunctionCall($phpcsFile, $stackPtr) && ! $helper->isMethodCall($phpcsFile, $stackPtr) && ! $helper->isBuiltInFunction($phpcsFile, $stackPtr) && ! $helper->isObjectInstantiation($phpcsFile, $stackPtr)) {
+			return $this->processFunctionCall($phpcsFile, $stackPtr);
+		}
+		if ($helper->isConstant($phpcsFile, $stackPtr) && ! $helper->isConstantDefinition($phpcsFile, $stackPtr) && ! $helper->isPredefinedConstant($phpcsFile, $stackPtr)) {
+			$this->processConstant($phpcsFile, $stackPtr);
+		}
+		if ($helper->isClass($phpcsFile, $stackPtr) && ! $helper->isPredefinedClass($phpcsFile, $stackPtr) && ! $helper->isPredefinedConstant($phpcsFile, $stackPtr)) {
+			$this->processClass($phpcsFile, $stackPtr);
+		}
+	}
+
+	private function processClass($phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		$className = $token['content'];
+		if (! $this->isClassImported($className) && ! $this->isClassDefined($phpcsFile, $className)) {
+			$error = "Classes must be explicitly imported. Found '{$className}'.";
+			$phpcsFile->addWarning($error, $stackPtr, 'Class');
+		}
+	}
+
+	private function processConstant($phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		$constantName = $token['content'];
+		if (! $this->isConstImported($constantName) && ! $this->isConstDefined($phpcsFile, $constantName)) {
+			$error = "Constants must be explicitly imported. Found '{$constantName}'.";
+			$phpcsFile->addWarning($error, $stackPtr, 'Constant');
+		}
+	}
+
+	private function processStaticFunctionCall(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$className = $helper->getStaticPropertyClass($phpcsFile, $stackPtr);
+		if (! $this->isClassImported($className)) {
+			$error = "Classes must be explicitly imported. Found '{$className}'.";
+			$phpcsFile->addWarning($error, $stackPtr, 'Class');
+		}
+	}
+
+	private function processFunctionCall(File $phpcsFile, $stackPtr) {
+		$tokens = $phpcsFile->getTokens();
+		$token = $tokens[$stackPtr];
+		$functionName = $token['content'];
+		if (! $this->isFunctionImported($functionName) && ! $this->isFunctionDefined($phpcsFile, $functionName)) {
+			$error = "Functions must be explicitly imported. Found '{$functionName}'.";
+			$phpcsFile->addWarning($error, $stackPtr, 'Function');
+		}
+	}
+
+	private function processUse(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$importType = $helper->getImportType($phpcsFile, $stackPtr);
+		switch ($importType) {
+			case 'function':
+				return $this->saveFunctionImport($phpcsFile, $stackPtr);
+			case 'const':
+				return $this->saveConstImport($phpcsFile, $stackPtr);
+			case 'class':
+				return $this->saveClassImport($phpcsFile, $stackPtr);
+		}
+	}
+
+	private function saveFunctionImport(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$importNames = $helper->getImportNames($phpcsFile, $stackPtr);
+		$this->importedFunctions = array_merge($this->importedFunctions, $importNames);
+	}
+
+	private function saveConstImport(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$importNames = $helper->getImportNames($phpcsFile, $stackPtr);
+		$this->importedConsts = array_merge($this->importedConsts, $importNames);
+	}
+
+	private function saveClassImport(File $phpcsFile, $stackPtr) {
+		$helper = new SniffHelpers();
+		$importNames = $helper->getImportNames($phpcsFile, $stackPtr);
+		$this->importedClasses = array_merge($this->importedClasses, $importNames);
+	}
+
+	private function isFunctionImported(string $functionName): bool {
+		return in_array($functionName, $this->importedFunctions);
+	}
+
+	private function isConstImported(string $constName): bool {
+		return in_array($constName, $this->importedConsts);
+	}
+
+	private function isClassImported(string $name): bool {
+		return in_array($name, $this->importedClasses);
+	}
+
+	private function isClassDefined(File $phpcsFile, string $className): bool {
+		$classPtr = $phpcsFile->findNext([T_CLASS], 0);
+		while ($classPtr) {
+			$thisClassName = $phpcsFile->getDeclarationName($classPtr);
+			if ($className === $thisClassName) {
+				return true;
+			}
+			$classPtr = $phpcsFile->findNext([T_CLASS], $classPtr + 1);
+		}
+		return false;
+	}
+
+	private function isFunctionDefined(File $phpcsFile, string $functionName): bool {
+		$functionPtr = $phpcsFile->findNext([T_FUNCTION], 0);
+		while ($functionPtr) {
+			$thisFunctionName = $phpcsFile->getDeclarationName($functionPtr);
+			if ($functionName === $thisFunctionName) {
+				return true;
+			}
+			$functionPtr = $phpcsFile->findNext([T_FUNCTION], $functionPtr + 1);
+		}
+		return false;
+	}
+
+	private function isConstDefined(File $phpcsFile, string $functionName): bool {
+		$helper = new SniffHelpers();
+		$functionPtr = $phpcsFile->findNext([T_CONST], 0);
+		while ($functionPtr) {
+			$thisFunctionName = $helper->getConstantName($phpcsFile, $functionPtr);
+			if ($functionName === $thisFunctionName) {
+				return true;
+			}
+			$functionPtr = $phpcsFile->findNext([T_CONST], $functionPtr + 1);
+		}
+		return false;
 	}
 }
