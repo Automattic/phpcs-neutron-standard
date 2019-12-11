4ba10b1bb7aca6915180587bddc29f20b2ccbd9c
Payton Swick <payton@foolord.com>
2019-12-11 18:16:15 -0500

    Add tests for maxFunctionLines

--- tests/Sniffs/Functions/LongFunctionSniffTest.php
+++ tests/Sniffs/Functions/LongFunctionSniffTest.php
@@ -21,4 +21,48 @@ class LongFunctionSniffTest extends TestCase {
 		$warnings = $helper->getWarningLineNumbersFromFile($phpcsFile);
 		$this->assertEquals([61], $warnings);
 	}
+
+	public function testLongFunctionWithDifferentLength() {
+		$fixtureFile = __DIR__ . '/LongFunctionsFixture.php';
+		$sniffFile =
+			__DIR__ .
+			'/../../../NeutronStandard/Sniffs/Functions/LongFunctionSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs(
+			$sniffFile,
+			$fixtureFile
+		);
+		$phpcsFile->ruleset->setSniffProperty(
+			'NeutronStandard\Sniffs\Functions\LongFunctionSniff',
+			'maxFunctionLines',
+			'50'
+		);
+		$phpcsFile->process();
+		$lines = $helper->getWarningLineNumbersFromFile($phpcsFile);
+		$expectedLines = [];
+		$this->assertEquals($expectedLines, $lines);
+	}
+
+	public function testLongFunctionWithShorterLength() {
+		$fixtureFile = __DIR__ . '/LongFunctionsFixture.php';
+		$sniffFile =
+			__DIR__ .
+			'/../../../NeutronStandard/Sniffs/Functions/LongFunctionSniff.php';
+
+		$helper = new SniffTestHelper();
+		$phpcsFile = $helper->prepareLocalFileForSniffs(
+			$sniffFile,
+			$fixtureFile
+		);
+		$phpcsFile->ruleset->setSniffProperty(
+			'NeutronStandard\Sniffs\Functions\LongFunctionSniff',
+			'maxFunctionLines',
+			'2'
+		);
+		$phpcsFile->process();
+		$lines = $helper->getWarningLineNumbersFromFile($phpcsFile);
+		$expectedLines = [3, 61];
+		$this->assertEquals($expectedLines, $lines);
+	}
 }
